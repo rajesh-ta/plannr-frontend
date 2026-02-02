8c86b3d1e2302c7ea97dce1ee2b3a0727c1c9f50
rajesh-ta <rajeshkumar.j@tigeranalytics.com>
2026-02-02 19:52:15 +0530

    Created theme

---
+++ src/theme/theme.ts
@@ -0,0 +1,16 @@
+"use client";
+
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#9775fa",
+    },
+    secondary: {
+      main: "#f06595",
+    },
+  },
+});
+
+export default theme;
